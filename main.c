cb73af9cf24ebec14dd90b9eee86994878f79209
ohwk123 <seojeong@sookmyung.ac.kr>
2022-09-20 17:51:29 +0900

    04

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	int input_int;
-	float input_float;
+int main(int argc, char *argv[]) {
+	int x,y;
+	float z;
 	
-	printf("enter an integer : ");
-	scanf("%d", &input_int);
+	printf("분자를 입력하시오:");
+	scanf("%d", &x);
+	printf("분모를 입력하시오:");
+	scanf("%d", &y);
+	z = (float)x/y;
 	
-	printf("enter a float :");
-	scanf("%f", &input_float);
 	
-	printf("integer : %d, float : %f\n", input_int, input_float);
-	
+	printf("나누기의 결과는 %f입니다\n", z );
 	return 0;
 }
