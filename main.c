4c6bf3967ccd60245a6528d4c5a0863af1a45631
ohwk123 <seojeong@sookmyung.ac.kr>
2022-09-20 17:58:12 +0900

    03

--- main.c
+++ main.c
@@ -5,10 +5,10 @@
 
 int main(int argc, char *argv[]) {
 	char text;
-	int i;
-	printf("input a number :");
+	
+	printf("input a character :");
 	scanf("%c", &text);
-	i = text - '0';
-	printf("The input number is %i\n", i);
+	
+	printf("The next character of %c (%d) is %c (%d)\n", text, text, text+1, text+1);
 	return 0;
 }
